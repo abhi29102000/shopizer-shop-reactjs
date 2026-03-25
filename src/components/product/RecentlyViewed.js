44481898cd80e925df0e56bd41e90fce566dca0d
Abhishek Sharma <abhisheksharma@teechnogises-MacBook-Pro.local>
2026-03-25 15:03:41 +0530

    Revert "feat: open ProductModal quick view on recently viewed item click"
    This reverts commit 6b3c4a4251299fa93ec692e79b5e707b0687b3f1.

--- src/components/product/RecentlyViewed.js
+++ src/components/product/RecentlyViewed.js
@@ -1,34 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchRecentlyViewed } from '../../redux/actions/recentlyViewedActions';
-import ProductModal from './ProductModal';
-import WebService from '../../util/webService';
-import constant from '../../util/constant';
 
-const RecentlyViewed = ({ defaultStore, currentLanguageCode }) => {
+const RecentlyViewed = () => {
   const dispatch = useDispatch();
   const recentlyViewed = useSelector(state => state.recentlyViewed);
   const items = recentlyViewed ? recentlyViewed.items : [];
-
-  const [modalShow, setModalShow] = useState(false);
-  const [selectedProduct, setSelectedProduct] = useState(null);
+  const loading = recentlyViewed ? recentlyViewed.loading : false;
 
   useEffect(() => {
     dispatch(fetchRecentlyViewed());
   }, [dispatch]);
 
-  const onQuickView = async (productId) => {
-    try {
-      const action = constant.ACTION.PRODUCTS + productId + '?lang=' + (currentLanguageCode || 'en') + '&store=' + (defaultStore || 'DEFAULT');
-      const product = await WebService.get(action);
-      if (product) {
-        setSelectedProduct(product);
-        setModalShow(true);
-      }
-    } catch (e) {}
-  };
-
-  if (!items || items.length === 0) return null;
+  if (loading || !items || items.length === 0) return null;
 
   return (
     <div className="recently-viewed-area pt-60 pb-60">
@@ -39,41 +24,25 @@ const RecentlyViewed = ({ defaultStore, currentLanguageCode }) => {
         <div className="row">
           {items.map(product => (
             <div key={product.id} className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 mb-25">
-              <div className="product-wrap" style={{ cursor: 'pointer' }} onClick={() => onQuickView(product.id)}>
-                {product.image && (
-                  <div className="product-img">
-                    <img src={product.image} alt={product.name} className="img-fluid" />
+              <Link to={`/product/${product.id}`}>
+                <div className="product-wrap">
+                  {product.image && (
+                    <div className="product-img">
+                      <img src={product.image} alt={product.name} className="img-fluid" />
+                    </div>
+                  )}
+                  <div className="product-content text-center mt-10">
+                    <h5>{product.name}</h5>
+                    {product.price && <span>${product.price.toFixed(2)}</span>}
                   </div>
-                )}
-                <div className="product-content text-center mt-10">
-                  <h5>{product.name}</h5>
-                  {product.price && <span>${product.price.toFixed(2)}</span>}
                 </div>
-              </div>
+              </Link>
             </div>
           ))}
         </div>
       </div>
-
-      {selectedProduct && (
-        <ProductModal
-          show={modalShow}
-          onHide={() => setModalShow(false)}
-          product={selectedProduct}
-          defaultStore={defaultStore || 'DEFAULT'}
-          finalproductprice={selectedProduct.price}
-          finaldiscountedprice={selectedProduct.price}
-          addtocart={() => {}}
-        />
-      )}
     </div>
   );
 };
 
-import { connect } from 'react-redux';
-const mapStateToProps = state => ({
-  defaultStore: state.merchantData.defaultStore,
-  currentLanguageCode: state.multilanguage.currentLanguageCode
-});
-
-export default connect(mapStateToProps)(RecentlyViewed);
+export default RecentlyViewed;
