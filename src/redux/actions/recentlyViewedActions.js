e8fe2d1e4528d9462da267b0bee8fd219a48a1ac
Abhishek Sharma <abhisheksharma@teechnogises-MacBook-Pro.local>
2026-03-25 14:54:04 +0530

    fix: use absolute URL + send auth token in recordView

--- src/redux/actions/recentlyViewedActions.js
+++ src/redux/actions/recentlyViewedActions.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 export const FETCH_RECENTLY_VIEWED = 'FETCH_RECENTLY_VIEWED';
 export const SET_RECENTLY_VIEWED = 'SET_RECENTLY_VIEWED';
 
+const BASE_URL = window._env_.APP_BASE_URL + window._env_.APP_API_VERSION;
+
 const getSessionId = () => {
   let id = localStorage.getItem('sessionId');
   if (!id) {
@@ -12,22 +14,24 @@ const getSessionId = () => {
   return id;
 };
 
+const getHeaders = () => {
+  const token = localStorage.getItem('token');
+  const headers = { 'X-Session-Id': getSessionId() };
+  if (token) headers['Authorization'] = 'Bearer ' + token;
+  return headers;
+};
+
 export const recordView = (productId) => () => {
-  axios.post(`products/${productId}/view`, null, {
-    headers: { 'X-Session-Id': getSessionId() }
-  }).catch(() => {});
+  axios.post(`${BASE_URL}products/${productId}/view`, null, { headers: getHeaders() })
+    .catch(() => {});
 };
 
 export const fetchRecentlyViewed = () => async (dispatch) => {
   dispatch({ type: FETCH_RECENTLY_VIEWED });
   try {
-    const token = localStorage.getItem('token');
-    const headers = { 'X-Session-Id': getSessionId() };
-    if (token) headers['Authorization'] = 'Bearer ' + token;
-
-    const response = await axios.get('customer/recently-viewed', {
+    const response = await axios.get(`${BASE_URL}customer/recently-viewed`, {
       params: { store: 'DEFAULT', lang: 'en' },
-      headers
+      headers: getHeaders()
     });
     dispatch({ type: SET_RECENTLY_VIEWED, payload: response.data });
   } catch (e) {
